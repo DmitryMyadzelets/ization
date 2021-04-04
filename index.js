b90f1006cd5695d4437aeb111e599ceedeca380e
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2021-04-04 13:11:33 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const isObject = o => 'object' === typeof o && o !== null
+
+// Returns a function to access the given map by the key
+// Returns the key back if a map is not given
+const t = map => isObject(map) ? key => map[key] || key : map
+
+// Returns a function to access the given dictionary by the language
+const dictionary = dic => language => t(dic[language]) || t
+
+module.exports = { t, dictionary }
